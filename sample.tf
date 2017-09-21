58b9fd2e1196b6707c319fb7678b7d7ec0f37f73
sudheerchamarthi <sudheer.chamarthi@gmail.com>
2017-09-21 22:29:57 +0530

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -1,3 +1,4 @@
+provider "aws" {
     region = "${var.INSTANCE_TYPE}"
 
 }
@@ -5,6 +6,8 @@ resource "aws_instance" "web" {
   instance_type = "${var.INSTANCE_TYPE}"
   ami = "${var.AMI_ID}"
   key_name = "${var.KEY_NAME}"
+
+
   # We set the name as a tag
   tags {
     "Name" = "MyTF1"
